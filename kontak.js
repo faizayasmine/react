b038d7269d1d891fba16b8ac5bec92ab49bebe5f
faizayasmine <133986141+faizayasmine@users.noreply.github.com>
2024-03-24 11:16:55 +0700

    kontak.js

---
+++ kontak.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+class Kontak extends React.Component {
+    render() {
+        return (
+            <div>
+                <section className='about'>
+                    <h1>Welcome To kontak Page</h1>
+                </section>
+            </div>
+        )
+    }
+}
+export default Kontak;
