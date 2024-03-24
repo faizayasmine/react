072b4ffa9f4d99f42bf36b4de619cca9ed924852
faizayasmine <133986141+faizayasmine@users.noreply.github.com>
2024-03-24 11:16:22 +0700

    karya.js

---
+++ karya.js
@@ -0,0 +1,13 @@
+import React from "react";
+class Karya extends React.Component{
+    render(){
+        return(
+            <div>
+              <section className='about'>
+        <h1>Welcome To karya Page</h1>
+      </section>
+            </div>
+        )
+    }
+}
+export default Karya;
