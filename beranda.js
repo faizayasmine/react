ec35facc2174710ecc1c77139969cf5d26209fe2
faizayasmine <133986141+faizayasmine@users.noreply.github.com>
2024-03-24 10:49:34 +0700

    beranda.js

---
+++ beranda.js
@@ -0,0 +1,14 @@
+import React from "react";
+class Beranda extends React.Component {
+    render() {
+        return (
+            <div>
+                <section className='about'>
+                    <h1>welcome to home page</h1>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default Beranda;
