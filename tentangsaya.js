0ad7a878aba9490c4af344697517166a5e142d9f
faizayasmine <133986141+faizayasmine@users.noreply.github.com>
2024-03-24 11:17:26 +0700

    tentangsaya.js

---
+++ tentangsaya.js
@@ -0,0 +1,15 @@
+import React from "react";
+class Tentangsaya extends React.Component{
+render(){
+    return(
+    <div>
+    <section className='about'>
+        <h1>Welcome To About Page</h1>
+      </section>
+        </div>
+)
+}
+}
+
+export default Tentangsaya;
+
